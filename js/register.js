4b94c9483cad05c474f6c59fa5adb5a2a1f8f75a
melaniebuergin <melanie.buergin@stud.fhgr.ch>
2026-04-29 15:00:13 +0200

    fix register

--- js/register.js
+++ js/register.js
@@ -3,18 +3,21 @@ console.log("register.js ist verbunden")
 document.getElementById("registerForm").addEventListener("submit", async (e) => {
 
     //hier schreiben wir, was beim submit passiert
-    e.preventDefaultDefault();
+    e.preventDefault();
     console.log("Submit!");
 
-    const email = document.getElementById("email").ariaValueMax.trim();
+    const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
     console.log(email +" " + password);
 
     try {
         const response = await fetch("api/register.php",{
             method: "POST",
-            headers: {"Content-Type": "application/x-www-form-urlencoded"},
-            body: new URLSearchParams({email, password}),
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({ email, password }),
         });
+
+        const result = await response.json();
+        console.log(result);
     } catch (error) {}
 });
