1691dd7c8d80afe944e18ac8fc18d2483ce3c423
melaniebuergin <melanie.buergin@stud.fhgr.ch>
2026-04-29 14:17:13 +0200

    update register.js save data

--- js/register.js
+++ js/register.js
@@ -4,5 +4,9 @@ document.getElementById("registerForm").addEventListener("submit", async (e) => 
 
     //hier schreiben wir, was beim submit passiert
     e.preventDefaultDefault();
-    console.log("Submit!")
+    console.log("Submit!");
+
+    const email = document.getElementById("email").ariaValueMax.trim();
+    const password = document.getElementById("password").value.trim();
+    console.log(email +" " + password);
 });
