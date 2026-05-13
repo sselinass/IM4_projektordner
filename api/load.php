bdf0613667f329e63422c7bbf29248f7685fb7b8
Selina Schöpfer <selina.schoepfer@stud.fhgr.ch>
2026-05-13 15:10:15 +0200

    load.php - delete timestamp

--- api/load.php
+++ api/load.php
@@ -5,7 +5,6 @@ $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 
 $buzzer_events = $input["buzzer_events"];
-$timestamp = $input["timestamp"];
 $id_users = $input["id_users"];
 
 $sql = "INSERT INTO NEU_buzzer_events (buzzer_events, timestamp, id_users) VALUES (?, ?, ?)";
@@ -13,7 +12,6 @@ $sql = "INSERT INTO NEU_buzzer_events (buzzer_events, timestamp, id_users) VALUE
 $stmt = $pdo->prepare($sql);
 $stmt->execute([
     $buzzer_events,
-    $timestamp,
     $id_users
 ]);
 
