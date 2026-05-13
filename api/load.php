6196caece14c3cf109868c573892bbe5462d57a0
Selina Schöpfer <selina.schoepfer@stud.fhgr.ch>
2026-05-13 15:35:32 +0200

    Update load.php

--- api/load.php
+++ api/load.php
@@ -1,10 +1,17 @@
 <?php
+/*******
+ * Kapitel 12: Website2DB > Schritt 2: Website -> DB
+ * load.php
+ * Daten als JSON-String vom ESP empfangen und in die Datenbank einfügen
+ ******/
+
 require_once("../system/config.php");
 
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 
 $buzzer_events = $input["buzzer_events"];
+$timestamp = $input["timestamp"];
 $id_users = $input["id_users"];
 
 $sql = "INSERT INTO NEU_buzzer_events (buzzer_events, timestamp, id_users) VALUES (?, ?, ?)";
@@ -12,10 +19,8 @@ $sql = "INSERT INTO NEU_buzzer_events (buzzer_events, timestamp, id_users) VALUE
 $stmt = $pdo->prepare($sql);
 $stmt->execute([
     $buzzer_events,
+    $timestamp,
     $id_users
 ]);
 
-echo json_encode([
-    "status" => "success"
-]);
 ?>
